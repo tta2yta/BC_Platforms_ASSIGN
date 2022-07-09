9627e87c98317aaa43b720096c3c25da488f2965
tta2yta <tta2yta@gmail.com>
2022-07-09 16:29:09 +0300

    add UserRole component to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import UserRoles from "./components/UserRoles";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <UserRoles />
+    </div>
+  );
 }
 
 export default App;
